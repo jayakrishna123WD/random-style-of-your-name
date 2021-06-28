6152da9e9e37017e9d5cd75a2ab3b393a5701b97
jayakrishna123WD <82320350+jayakrishna123WD@users.noreply.github.com>
2021-06-28 17:11:59 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,151 @@
+
+            //style1
+            function design1() {
+                var style1 = document.querySelector('#hod1').style;
+                var name = document.getElementById("name").value;
+                document.querySelector('#hod1').innerHTML = name;
+                style1.color = "black";
+                style1.borderLeft = "9px solid black";
+                style1.borderRadius = "3%";
+                style1.padding = "6%";
+                style1.background = "linear-gradient(45deg,#654ea3,#eaafc8)";
+                style1.maxWidth = "87%";
+                style1.margin = "10%";
+                style1.transform = "rotate(-10deg)";
+                style1.fontFamily =  'Dancing Script', cursive;
+            }
+            //style2
+            function design2() {
+                var style2 = document.querySelector('#hod2').style;
+                var name = document.getElementById("name").value;
+                document.querySelector('#hod2').innerHTML = name;
+                style2.textShadow = "5px 20px 6px Purple";
+                style2.textAlign = "center";
+                style2.color = "white";
+                style2.transform = "skew(20deg,10deg)";
+                style2.margin = "10% 5% 20% 5%";
+                style2.maxWidth = "80%";
+                style2.padding = "3%";
+                style2.fontFamily = 'Kaushan Script', cursive;
+            }
+            //style3
+            function design3() {
+                var style3 = document.querySelector('#hod3').style;
+                var name = document.getElementById("name").value;
+                document.querySelector('#hod3').innerHTML = name;
+                style3.borderRight = "9px solid #ffafcc";
+                style3.borderBottom = "9px solid #ffafcc";
+                style3.textAlign = "center";
+                style3.borderRadius = "2%";
+                style3.Width = "70%";
+                style3.transform = "skew(30deg,-10deg)";
+                style3.textShadow = "5px 12px 6px #fee440";
+                style3.margin = "10% 5% 20% 5%";
+                style3.padding = "3%";
+                style3.fontFamily = 'Kalam', cursive;
+            }
+            //style4
+            function design4() {
+                var style4 = document.querySelector('#hod4').style;
+                var name = document.getElementById("name").value;
+                document.querySelector('#hod4').innerHTML = name;
+                style4.color = "#fee440";
+                style4.border = "4px ridge #cddafd";
+                style4.boxShadow = "5px 12px 6px #f9c74f";
+                style4.textAlign = "center";
+                style4.letterSpacing = "6px";
+                style4.margin = "10% 5% 20% 5%";
+                style4.maxWidth = "80%";
+                style4.padding = "3%";
+                style4.fontFamily = 'Londrina Shadow', cursive;
+            }
+            //style5
+            function design5() {
+                var style5 = document.querySelector('#hod5').style;
+                var name = document.getElementById("name").value;
+                document.querySelector('#hod5').innerHTML = name;
+                style5.color = "white";
+                style5.textShadow = "5px 12px 6px #0353a4";
+                style5.borderTop = "6px dashed #002855";
+                style5.boxShadow = "5px 12px 6px #0353a4"; 
+                style5.margin = "10% 5% 20% 5%";
+                style5.maxWidth = "80%";
+                style5.padding = "3%";
+                style5.fontFamily = 'Bungee Shade', cursive;
+            }
+            //style6
+            function design6() {
+                var style6 = document.querySelector('#hod6').style;
+                var name = document.getElementById("name").value;
+                document.querySelector('#hod6').innerHTML = name;
+                style6.color = "#0a2472";
+                style6.borderBottom = "8px dotted #f4acb7";
+                style6.borderLeft = "8px dotted #f4acb7";
+                style6.borderRadius = "3%";
+                style6.boxShadow = "5px 12px 6px #d100d1";
+                style6.transform = "rotate(10deg)";
+                style6.margin = "10% 5% 20% 5%";
+                style6.maxWidth = "80%";
+                style6.padding = "3%";
+                style6.fontFamily = 'Megrim', cursive;
+            }
+            //style7
+            function design7() {
+                var style7 = document.querySelector('#hod7').style;
+                var name = document.getElementById("name").value;
+                document.querySelector('#hod7').innerHTML = name;
+                style7.color = "#000";
+                style7.letterSpacing = "6px";
+                style7.textAlign = "right";
+                style7.transform = "skew(-20deg,10deg)";
+                style7.textShadow = "5px 12px 6px #dd1c1a";
+                style7.margin = "10% 5% 20% 5%";
+                style7.padding = "3%";
+                style7.fontFamily = 'Akronim', cursive;
+            }
+            //style8
+            function design8() {
+                var style8 = document.querySelector('#hod8').style;
+                var name = document.getElementById("name").value;
+                document.querySelector('#hod8').innerHTML = name;
+                style8.color = "#6247aa";
+                style8.border = "5px inset black";
+                style8.borderRadius = "10px";
+                style8.background = "#d6e2e9";
+                style8.transform = "rotate(10deg)";
+                style8.transform = "skew(10deg,20deg)";
+                style8.margin = "10% 5% 20% 5%";
+                style8.maxWidth = "90%";
+                style8.padding = "4%";
+                style8.fontFamily = 'Monoton', cursive;
+            }
+            //style9
+            function design9() {
+                var style9 = document.querySelector('#hod9').style;
+                var name = document.getElementById("name").value;
+                document.querySelector('#hod9').innerHTML = name;
+                style9.color = "#1d3557";
+                style9.textShadow = "5px 12px 6px #6930c3";
+                style9.textAlign = "center";
+                style9.transform = "skew(-20deg,10deg)";
+                style9.margin = "10% 5% 20% 5%";
+                style9.maxWidth = "80%";
+                style9.padding = "3%";
+                style9.fontFamily = 'Calligraffitti', cursive;
+            }
+            //style 10
+            function design10() {
+                var style10 = document.querySelector('#hod10').style;
+                var name = document.getElementById("name").value;
+                document.querySelector('#hod10').innerHTML = name;
+                style10.color = "#000";
+                style10.textShadow = "20px 20px 10px #e5e5e5";
+                style10.letterSpacing = "10px";
+                style10.transform = "rotate(-15deg)";
+                style10.textAlign = "center";
+                style10.transform = "skew(-10deg,-20deg)";
+                style10.margin = "10% 5% 20% 5%";
+                style10.maxWidth = "80%";
+                style10.padding = "3%";
+                style10.fontFamily = 'Fredericka the Great', cursive;
+            }
